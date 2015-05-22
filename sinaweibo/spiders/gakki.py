f7ca788c08b2c1de1e6d7a57474268bdb7e86005
yixuan zhao <120559187@qq.com>
2015-05-22 19:39:55 +0800

    日NMLGB
    好不容易照猫画虎写出这个formrequest，账号密码储存在formdata里，发现爬微博会出现找不到<form>元素，搞半天都不行，爬SG的话就不会报错……另外"username"和"password"好像大多数网站都通用

--- sinaweibo/spiders/gakki.py
+++ sinaweibo/spiders/gakki.py
@@ -1,27 +1,28 @@
 # -*- coding: UTF-8 -*-
 from scrapy.spider import Spider
-
+import scrapy
 from sinaweibo.items import Blog  # text.time
 
 
 class GakkiSpider(Spider):
     name = "gakki"
-    allowed_domains = ["http://weibo.com/"]
-    start_urls = ["http://weibo.com/aragakiyui0611"]
+    allowed_domains = ["http://bbs.sgamer.com"]
+    start_urls = ["http://bbs.sgamer.com/forum-44-1.html"]#妈个比，微博换SG就不会出现没有form元素了
 
     def load_item(self, d):
         return d
 
     def parse(self, response):
-        items = []
-        item = Blog()
+        return scrapy.FormRequest.from_response(
+            response,
+            formdata={'username': '120559187@qq.com', 'password': 'yixuanzyx'},
+            callback=self.after_login
+        )
 
-        print "loooooooooooooooooooook"
-        sites = response.xpath('//tr')
-        for sel in sites:
-
-            item['text']=response.xpath('/div[@class="mcon fl"]/text()').extract()
-            items.append(item)
-        for d in items:
-            yield self.load_item(d)
+    def after_login(self, response):
+        print "looooooooooooooooooooook"
+        print response.body
+        # items=[]
+        #for d in items:
+        #    yield self.load_item(d)
         return
