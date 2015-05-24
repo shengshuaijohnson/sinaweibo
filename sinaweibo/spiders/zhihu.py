081f414b39209801264ebd2d5ed38d91e671c5ee
yixuan zhao <120559187@qq.com>
2015-05-24 19:05:08 +0800

    放你妈的血！！我说为什么yield Request老报错！！
    from scrapy.http import Request
    from scrapy.http import Request
    from scrapy.http import Request
    ！！！！！！！！！！！！！！！！！！！！！！！！！！！！
    callback=self.parse
    或者转到另外一个也行！
    奇怪的是哪怕不设IF条件他最多也就重复一次，不会无限循环。

--- sinaweibo/spiders/zhihu.py
+++ sinaweibo/spiders/zhihu.py
@@ -1,18 +1,27 @@
 __author__ = 'Administrator'
 # -*- coding: utf-8 -*-
 from scrapy.spider import Spider
-import scrapy
 from sinaweibo.items import Blog
+from scrapy.http import Request
+
+
+def load_item(d):
+    return d
+
 
 class Zhihuspider(Spider):
     name = "zhihu"
-    allowed_domains = ["http://www.zhihu.com/"]
+    # allowed_domains = ["http://www.zhihu.com/"]
     start_urls = [
-        "http://www.zhihu.com/"
+        "http://www.zhihu.com/#signin"
     ]
 
     def parse(self, response):
-        site = response.body
-        item=Blog()
-        item["text"]=site
-        return item
+        site = response.xpath("//text()").extract()
+        item = Blog()
+        item["text"] = site
+
+        if response.url == "http://www.zhihu.com/":
+            yield Request("http://bbs.sgamer.com/forum-44-2.html", callback=self.parse)
+        yield load_item(item)
+        return
